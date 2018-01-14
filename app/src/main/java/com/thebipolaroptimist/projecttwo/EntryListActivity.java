4598ea1cf3d8ad5b852df6ac343e73f28d6a27cf
Bethany <no1isnothing@gmail.com>
2018-01-14 16:54:42 -0600

    first commit of project two. navigation menu opens if you swipe, but there's not a button for it on top yet. also, the activity doesn't display or do anything yet.

---
+++ app/src/main/java/com/thebipolaroptimist/projecttwo/EntryListActivity.java
@@ -0,0 +1,14 @@
+package com.thebipolaroptimist.projecttwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntryListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_entry_list);
+        super.onCreate(savedInstanceState);
+
+    }
+}
