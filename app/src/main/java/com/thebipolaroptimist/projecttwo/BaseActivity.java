fc1d2ed7564569adcf5ab9b2d8660fe8febf26ae
Bethany <no1isnothing@gmail.com>
2018-01-24 09:56:01 -0600

    created 2 activities implementing a single base activity that handles the nav drawer and tool bar. navigation between activities is working. activities don't do anything besides display text yet.

--- app/src/main/java/com/thebipolaroptimist/projecttwo/BaseActivity.java
+++ app/src/main/java/com/thebipolaroptimist/projecttwo/BaseActivity.java
@@ -1,15 +1,110 @@
 package com.thebipolaroptimist.projecttwo;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class BaseActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mDrawerList;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (NavigationView) findViewById(R.id.navigation);
+        mDrawerList.setNavigationItemSelectedListener(new DrawerItemOnClickListener());
 
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setHomeButtonEnabled(true);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    /*
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }*/
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private class DrawerItemOnClickListener implements NavigationView.OnNavigationItemSelectedListener
+    {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int itemId = item.getItemId();
+
+            switch(itemId)
+            {
+                case R.id.nav_entry_list:
+                    Intent intent = new Intent(getApplicationContext(), EntryListActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_second_page:
+                    Intent intent2 = new Intent(getApplicationContext(), SecondActivity.class);
+                    startActivity(intent2);
+                    break;
+
+            }
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/com/thebipolaroptimist/projecttwo/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.thebipolaroptimist.projecttwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_second);
+        super.onCreate(savedInstanceState);
+    }
+}
